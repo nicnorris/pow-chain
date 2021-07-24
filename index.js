015191796c854631eb8ad67bff51aa3683ef8cd6
nicnorris <nicnorrismail@gmail.com>
2021-07-23 17:06:45 -0700

    day 1

---
+++ index.js
@@ -0,0 +1,20 @@
+const jayson = require('jayson');
+const {startMining, stopMining} = require('./mine');
+const {PORT} = require('./config');
+
+// create a server
+const server = new jayson.server({
+  startMining: function(_, callback) {
+    callback(null, 'success!');
+    startMining();
+  },
+
+ stopMining: function(_, callback) {
+    callback(null, 'success!');
+    stopMining();
+  }
+});
+
+server.http().listen(PORT);
+
+
---
+++ scripts/stopMining.js
@@ -0,0 +1,7 @@
+const client = require('./client');
+
+
+client.request('stopMining', [], function(err, response) {
+  if(err) throw err;
+  console.log(response.result); // success!
+});
